605fc56082dcbf9b41e0959e26f7bacbee3e4f62
OverLordGoldDragon <16495490+OverLordGoldDragon@users.noreply.github.com>
2020-06-03 00:43:52 +0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,3 +53,7 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# ReadTheDocs sets master doc to index.rst, whereas Sphinx expects it to be
+# contents.rst:
+master_doc = 'index'
