390e6e12c3094bc0cf9d499c9177de1029b830b9
OverLordGoldDragon <john.muradeli@gmail.com>
2023-01-04 16:06:27 +0400

    y

--- wavespin/backend/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# -----------------------------------------------------------------------------
-# Copyright (c) 2022- John Muradeli
-#
-# Distributed under the terms of the MIT License
-# (see wavespin/__init__.py for details)
-# -----------------------------------------------------------------------------
